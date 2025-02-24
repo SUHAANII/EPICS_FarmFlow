914bfa446487a771c27a449706e433d8dc0f5aa5
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-02-24 22:16:28 +0530

    first commit by deepak

--- backend/src/models/Technique.js
+++ backend/src/models/Technique.js
@@ -4,7 +4,7 @@ const TechniqueSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   coverImage: { type: String, required: true },
   title: { type: String, required: true },
-  subtitle: { type: String },
+  subtitle: { type: String , required: true },
   description: { type: String, required: true },
   tags: [String],
   comments: [{ user: String, text: String, date: { type: Date, default: Date.now } }],
