17a8056ee67d8f6f965bf298a40a33ec11e25597
rriddhi-04 <riddhimabhowmick@gmail.com>
2025-03-30 13:40:57 +0530

    modified app.jsx and added padding on everypage without home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,90 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import Navbar from "./components/navbar/Navbar";
+// import Footer from "./components/footer/Footer";
+// import Home from "./components/home/Home";  
+// import {Login } from "./components/login/Login";
+// import Register from "./components/register/Register";
+// import Techniques from "./components/techniques/Techniques";
+// import UserInfo from "./components/user/UserInfo";
+// import BlogPage from "./components/blogs/blog";
+// // import FetchData from "./components/Fetchdata";
+// // import SendData from "./components/SendData";
+// // import UpdateData from "./components/Update";
+
+// function App() {
+//   return (
+//     <>
+
+
+//     <div>
+//       {/* <h1>Farm-Flow Integration</h1> */}
+//       {/* <FetchData />
+//       <SendData />
+//       <UpdateData /> */}
+//     </div>
+//     <Router>
+//       <Navbar />
+//       <main style={{ paddingTop: "80px" }}>
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//             <Route path="/register" element={<Register />} />
+//             <Route path="/techniques" element={<Techniques />} style={{padding: "84px"}} />
+//             <Route path="/userinfo" element={<UserInfo />} />
+//             <Route path="/blog" element={<BlogPage />} />
+//             {/* <Route path="/forgot-password" element={<ForgotPasswordPage />} />   */}
+//           <Route path="/" element={<Home/>} />
+//         </Routes>
+//       </main>
+//       <Footer />
+//     </Router>
+//     </>
+//   );
+// }
+
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";  
-import {Login } from "./components/login/Login";
+import { Login } from "./components/login/Login";
 import Register from "./components/register/Register";
 import Techniques from "./components/techniques/Techniques";
 import UserInfo from "./components/user/UserInfo";
 import BlogPage from "./components/blogs/blog";
-// import FetchData from "./components/Fetchdata";
-// import SendData from "./components/SendData";
-// import UpdateData from "./components/Update";
 
 function App() {
   return (
-    <>
-
-
-    <div>
-      {/* <h1>Farm-Flow Integration</h1> */}
-      {/* <FetchData />
-      <SendData />
-      <UpdateData /> */}
-    </div>
     <Router>
       <Navbar />
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/techniques" element={<Techniques />} />
-            <Route path="/userinfo" element={<UserInfo />} />
-            <Route path="/blog" element={<BlogPage />} />
-            {/* <Route path="/forgot-password" element={<ForgotPasswordPage />} />   */}
-          <Route path="/" element={<Home/>} />
-        </Routes>
-      </main>
+      <MainContent />
       <Footer />
     </Router>
-    </>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+  const applyPadding = location.pathname !== "/"; // Apply padding to all pages except Home
+
+  return (
+    <main style={{ paddingTop: applyPadding ? "80px" : "0px" }}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/techniques" element={<Techniques />} />
+        <Route path="/userinfo" element={<UserInfo />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
 export default App;
+
